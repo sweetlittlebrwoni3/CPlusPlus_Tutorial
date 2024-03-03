704052dd3f28b48e5ee9b66a8e6cfef20e6e84f8
m.pouya@nisys.ir <m.pouya@nisys.ir>
2024-03-03 13:25:48 +0330

    first commit

---
+++ Session9/FunctionOverloading.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void show(int val){printf("integer: %d\n",val);}
+void show(double val){printf("Double: %f\n",val);}
+void show(char const *val){printf("String: %s\n",val);}
+
+int main()
+{
+    show(12);
+    show(3.1415);
+    show("Hello World!\n");
+}
+
+/*But we cannot write a function such as:
+int show()*/
+
+
+//2 Important notes:
+
+/*-Do not use function overloading for functions
+doing coneptually different tasks.*/
+
+/*-C++ does not allow identically named functions to
+differ only in thei return values.*/
---
+++ Session9/StrictTypeChecking.cpp
@@ -0,0 +1,23 @@
+//ok in c error in c++
+int main()
+{
+
+    printf("Hello World!");
+}
+
+//ok in C++
+#include<stdio.h>
+int main()
+{
+    printf("Hello World!");
+}
+
+int main()
+{
+    //Error in C++: return-statement with no value, in function returning 'int'
+    return;
+}
+
+//ok in C++
+int main(){}
+
